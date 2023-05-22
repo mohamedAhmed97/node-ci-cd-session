194b8b850c0c7e4c8e0ba24c0278018fb6fd7c6d
mohamedAhmed97 <mohamed.ramadan@objects.email>
2023-05-22 23:33:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+
+
+app.get('/', function (req, res) {
+    res.send("HI , It's Node js App");
+});
+
+app.listen(3000, function () {
+    console.log("app listening on port 3000!");
+});
